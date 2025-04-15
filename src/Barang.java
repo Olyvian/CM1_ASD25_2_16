95df3649f1725fbef4ae624fba73a3abeacf7954
Olyvian <113090307+Olyvian@users.noreply.github.com>
2025-04-15 09:17:26 +0700

    new

--- src/Barang.java
+++ src/Barang.java
@@ -1,5 +1,9 @@
 public class Barang {
     String kodebarang,nama,kategori;
     int stok,harga;
+    void tampilDataBarang(){
+        System.out.printf("|%-5s | %-12s | %-6s | %-6s | %-13s |\n",kodebarang,nama
+        ,stok,harga);    
+    }
     
 }
--- src/Transaksi_Pembelian.java
+++ src/Transaksi_Pembelian.java
@@ -1,3 +1,5 @@
 public class Transaksi_Pembelian {
-    
+    String kodeTransaksi,namaPembeli,tanggalPembelian;
+    int qty;
+    Barang brg;
 }
